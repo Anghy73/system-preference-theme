0023d8b470d94516bb00389fdf4d5032c27c67b2
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2024-03-01 17:48:51 -0300

    made without tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,57 @@
+import { useEffect } from "react"
 
 function App() {
-  const handleClickScheme = () => {
-    const theme = window.matchMedia('(prefers-color-scheme: light)').matches
-    console.log(theme);
+
+  useEffect(() => {
+    handleClickScheme(window.localStorage.getItem('theme'))
+  }, [])
+
+  const handleClickLight = () => {
+    const newTheme = 'light'
+    window.localStorage.setItem('theme', newTheme)
+    handleClickScheme(newTheme)
   }
+
+  const handleClickDark = () => {
+    const newTheme = 'dark'
+    window.localStorage.setItem('theme', newTheme)
+    handleClickScheme(newTheme)
+  }
+
   const handleClickSystem = () => {
-    console.log('system');
+    const newTheme = ''
+    window.localStorage.setItem('theme', '')
+    handleClickScheme(newTheme)
+  }
+
+  const handleClickScheme = (theme) => {
+
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+      document.documentElement.classList.remove('os-default')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (theme === 'light') {
+      document.documentElement.classList.add('light')
+      document.documentElement.classList.remove('os-default')
+    } else {
+      document.documentElement.classList.remove('light')
+    }
+
+    if (theme === '' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add('os-default')
+      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove('light')
+    }
   }
 
   return (
     <main className="main">
-      <h1>Hello World</h1>
-      <button className="buttonTheme" onClick={handleClickScheme}>Change Theme</button>
-      <button className="buttonTheme" onClick={handleClickSystem}>Change Theme to System</button>
+      <button className="buttonTheme" onClick={handleClickDark}>Theme Dark</button>
+      <button className="buttonTheme" onClick={handleClickLight}>Theme Light</button>
+      <button className="buttonTheme" onClick={handleClickSystem}>Theme System</button>
     </main>
   )
 }
